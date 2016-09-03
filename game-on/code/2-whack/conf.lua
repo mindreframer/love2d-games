50106406db172cc296707e77810f107b24cfe0f7
Walter Stanish <walter@stani.sh>
2016-09-03 12:06:43 +0800

    Correct version

--- game-on/code/2-whack/conf.lua
+++ game-on/code/2-whack/conf.lua
@@ -1,9 +1,3 @@
 function love.conf(t)
-	t.title = "Whack a Knoll"
-	t.author = "vrld"
-	t.modules.joystick = false
-	t.modules.audio = false
-	t.modules.keyboard = false
-	t.modules.sound = false
-	t.modules.physics = false
+	t.version = "0.7.2"
 end
